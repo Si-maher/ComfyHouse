646f12c27e5fd0749b3b03661e32a82a4d9e725b
Simon Maher <simonmaher8307@hotmail.com>
2019-12-16 13:18:23 +0200

    created async function to fetch product

--- index.js
+++ index.js
@@ -17,7 +17,23 @@ let cart = []
 // Getting the products 
 
 class Products {
-
+async getProducts() {
+    try {
+        let result = await fetch("products.json")
+        let data = result.json()
+        let products = data.items;
+        products = products.map(item => {
+            const {title, price} = item.fields
+            const {id} = item.sys
+            const image = item.fields.image.fields.file.url 
+            return {title, price, id, image}
+        })
+        return products
+    }catch(error) {
+        console.log(error);
+        
+    }
+}
 }
 
 // displaying the products 
